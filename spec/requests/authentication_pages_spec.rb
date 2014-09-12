4ca2649a3ad84af8d848b1acaddc9481843c0237
Minsoo <minsoo91@gmail.com>
2014-09-12 15:10:57 +0900

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -65,23 +65,36 @@ describe "Authentication" do
   				end
   			end
 
-      		describe "in the Users controller" do
+    		describe "in the Users controller" do
 
-        		describe "visiting the edit page" do
-          			before { visit edit_user_path(user) }
-          			it { should have_title('Sign in') }
-       			end
-
-        		describe "submitting to the update action" do
-          			before { patch user_path(user) }
-          			specify { expect(response).to redirect_to(signin_path) }
-        		end
-
-        		describe "visiting the user index" do
-        			before { visit users_path }
+      		describe "visiting the edit page" do
+        			before { visit edit_user_path(user) }
         			it { should have_title('Sign in') }
-        		end
+     			end
+
+      		describe "submitting to the update action" do
+        			before { patch user_path(user) }
+        			specify { expect(response).to redirect_to(signin_path) }
       		end
+
+      		describe "visiting the user index" do
+      			before { visit users_path }
+      			it { should have_title('Sign in') }
+      		end
+    		end
+
+        describe "in the Microposts controller" do
+
+          describe "submitting to the create action" do
+            before { post microposts_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
     	end
 
     	describe "as wrong user" do
